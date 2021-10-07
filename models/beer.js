388a24f864def1b3f6398ae7fa26fcbf49bf3279
Jake <jakeefuller@gmail.com>
2021-10-07 17:36:33 -0400

    Added mongoose schema for beers

--- models/beer.js
+++ models/beer.js
@@ -3,6 +3,43 @@
 
 const mongoose = require("mongoose");
 
-const beerSchema = new mongoose.Schema({});
+const beerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  style: {
+    type: String,
+    required: true,
+  },
+  abv: {
+    type: Number,
+    required: false,
+  },
+  ibu: {
+    type: Number,
+    required: false,
+  },
+  brewery_name: {
+    type: String,
+    required: true,
+  },
+  brewery_city: {
+    type: String,
+    required: true,
+  },
+  brewer_country: {
+    type: String,
+    required: true,
+  },
+  bid: {
+    type: Number,
+    required: false,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("Beer", beerSchema, "beers");
